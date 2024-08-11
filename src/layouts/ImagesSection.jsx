a9057e61f49716755f1e72d7f236370a24d64eaa
bikramia <bikram.rhce@gmail.com>
2024-08-10 22:11:09 -0400

    updated svg and png - imageSection

--- src/layouts/ImagesSection.jsx
+++ src/layouts/ImagesSection.jsx
@@ -1,11 +1,11 @@
-import g1 from "@/assets/images/g1.svg";
-import g2 from "@/assets/images/g2.svg";
+import g1 from "@/assets/images/g1.png";
+import g2 from "@/assets/images/g2.png";
 import g3 from "@/assets/images/g3.svg";
-import g4 from "@/assets/images/g4.svg";
-import g5 from "@/assets/images/g5.svg";
-import g6 from "@/assets/images/g6.svg";
-import g7 from "@/assets/images/g7.svg";
-import g8 from "@/assets/images/g8.svg";
+import g4 from "@/assets/images/g4.png";
+import g5 from "@/assets/images/g5.png";
+import g6 from "@/assets/images/g6.png";
+import g7 from "@/assets/images/g7.png";
+import g8 from "@/assets/images/g8.png";
 
 const ImagesSection = () => {
   return (
@@ -48,6 +48,18 @@ const ImagesSection = () => {
               className="h-full w-full object-cover hover:scale-110 grayscale hover:grayscale-0 transition duration-300"
             />
           </a>
+          <a
+            className="overflow-hidden rounded-xl h-full"
+            href="http://www.amazon.com/shops/Abode-Zen"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={g1}
+              alt=""
+              className="h-full w-full object-cover hover:scale-110 grayscale hover:grayscale-0 transition duration-300"
+            />
+          </a>
         </div>
         <a
           className="overflow-hidden rounded-xl"
@@ -56,7 +68,7 @@ const ImagesSection = () => {
           rel="noreferrer"
         >
           <img
-            src={g5}
+            src={g4}
             alt=""
             className="w-full h-full grayscale hover:scale-110 hover:grayscale-0 transition duration-300"
           />
@@ -68,7 +80,7 @@ const ImagesSection = () => {
           rel="noreferrer"
         >
           <img
-            src={g7}
+            src={g5}
             alt=""
             className="h-full w-full grayscale hover:scale-110 hover:grayscale-0 transition duration-300"
           />
@@ -80,7 +92,7 @@ const ImagesSection = () => {
           rel="noreferrer"
         >
           <img
-            src={g4}
+            src={g6}
             alt=""
             className="h-full w-full grayscale hover:scale-110 hover:grayscale-0 transition duration-300"
           />
@@ -198,7 +210,6 @@ const ImagesSection = () => {
               className="h-full w-full object-cover hover:scale-110 grayscale hover:grayscale-0 transition duration-300"
             />
           </a>
-          
           <a
             className="overflow-hidden rounded-xl h-full"
             href="https://www.awwwards.com/"
@@ -207,6 +218,18 @@ const ImagesSection = () => {
           >
             <img
               src={g3}
+              alt=""
+              className="h-full w-full object-cover hover:scale-110 grayscale hover:grayscale-0 transition duration-300"
+            />
+          </a>
+          <a
+            className="overflow-hidden rounded-xl h-full"
+            href="https://www.awwwards.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={g1}
               alt=""
               className="h-full w-full object-cover hover:scale-110 grayscale hover:grayscale-0 transition duration-300"
             />

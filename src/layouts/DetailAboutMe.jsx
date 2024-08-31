dd9517e415816b4a637d74341f21587b8a859629
bikramia <bikram.rhce@gmail.com>
2024-08-30 21:27:43 -0400

    Added Tech stacks -DetailAboutMe Section

--- src/layouts/DetailAboutMe.jsx
+++ src/layouts/DetailAboutMe.jsx
@@ -33,6 +33,7 @@ import t36 from "@/assets/images/stack-icons/image26.svg";
 import t37 from "@/assets/images/stack-icons/image27.svg";
 import t38 from "@/assets/images/stack-icons/image28.svg";
 import t39 from "@/assets/images/stack-icons/image29.png";
+import t40 from "@/assets/images/stack-icons/image30.svg";
 import f1 from "@/assets/images/f1.svg";
 import f2 from "@/assets/images/f2.svg";
 import f3 from "@/assets/images/f3.svg";
@@ -200,6 +201,11 @@ const images = [
     id: 29,
     img: t39,
     title: "NETLIFY",
+  },
+  {
+    id: 30,
+    img: t40,
+    title: "INTELLIJ",
   },
 ];
 
